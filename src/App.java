b0f059dac8d4bcc992b3b502a7adfa722d3a38fb
Migox123 <miguelangelholguin8@gmail.com>
2026-07-07 02:07:39 -0500

    ver 3

--- src/App.java
+++ src/App.java
@@ -2,7 +2,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         byte numeroPequeño = 100;
         short numeroMediano =  32767;
+        int numeroGrande = 2147483647;
+        long numeroMuyGrande = 1;
+
+        System.out.println("El valor de numeroPequeño es: " + numeroPequeño);
+        suma de numeros = numeroPequeño x numeroGrande;
+        system.out.println("El valor de suma de numeros es: " + suma de numeros);
         
+
+
+
+
+
+
 
     }
 }
