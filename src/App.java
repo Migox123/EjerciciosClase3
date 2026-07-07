bb2ae09e4178a3f4c5243ec9c6cc7d7d895768e7
Migox123 <miguelangelholguin8@gmail.com>
2026-07-07 02:15:26 -0500

    v3

--- src/App.java
+++ src/App.java
@@ -1,13 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         byte numeroPequeño = 100;
-        short numeroMediano =  32767;
-        int numeroGrande = 2147483647;
-        long numeroMuyGrande = 1;
+        short numeroMediano =  10000; // inicializando
+        int numeroGrande ; // declarando
 
-        System.out.println("El valor de numeroPequeño es: " + numeroPequeño);
-        suma de numeros = numeroPequeño x numeroGrande;
-        system.out.println("El valor de suma de numeros es: " + suma de numeros);
+        var numeroMuyGrande = 10000; // java escoge el tipo de dato necesario para almacenar el valor
+        var n1 = 34;
+
+        long numeroEnorme = 10000l ;
+
+
+
+      
         
 
 
